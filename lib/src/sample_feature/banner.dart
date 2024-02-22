c74487ba63187e188cb5dd8e1e5fd026114fe7cc
Hardi <hardisyafrizal@gmail.com>
2024-02-22 10:13:35 +0700

    Feat: get location device

--- lib/src/sample_feature/banner.dart
+++ lib/src/sample_feature/banner.dart
@@ -1,16 +1,56 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
-class BannerWidget extends StatelessWidget {
-  final String imageUrl;
-  final String title;
+class BannerWidget extends StatefulWidget {
+  @override
+  _BannerWidgetState createState() => _BannerWidgetState();
+}
 
-  BannerWidget({required this.imageUrl, required this.title});
+class _BannerWidgetState extends State<BannerWidget> {
+  final List<Map<String, String>> banners = [
+    {"imageUrl": "assets/images/couple.jpeg", "title": "Banner 1"},
+    {"imageUrl": "assets/images/fire.jpeg", "title": "Banner 2"},
+    {"imageUrl": "assets/images/foye.png", "title": "Banner 3"},
+    // Add more banners as needed
+  ];
+
+  int _currentIndex = 0;
+  PageController _pageController = PageController();
+  Timer? _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(Duration(seconds: 3), (Timer timer) {
+      if (_currentIndex < banners.length - 1) {
+        _currentIndex++;
+      } else {
+        _currentIndex = 0;
+      }
+
+      Future.delayed(Duration(milliseconds: 100), () {
+        if (_pageController.hasClients) {
+          _pageController.animateToPage(
+            _currentIndex,
+            duration: Duration(milliseconds: 300),
+            curve: Curves.easeIn,
+          );
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+      margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10.0),
         boxShadow: [
@@ -25,36 +65,77 @@ class BannerWidget extends StatelessWidget {
       child: ClipRRect(
         borderRadius: BorderRadius.circular(10.0),
         child: Stack(
+          alignment: Alignment.bottomCenter,
           children: [
-            Image.asset(imageUrl),
-            Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: Container(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.bottomCenter,
-                    end: Alignment.topCenter,
-                    colors: [
-                      Colors.black.withOpacity(0.7),
-                      Colors.transparent,
-                    ],
-                  ),
-                ),
-                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                child: Text(
-                  title,
-                  style: TextStyle(
-                    color: Colors.red.shade200,
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
+            _bannerImages(),
+            _bannerIndicators(),
+            // Positioned(
+            //   bottom: 0,
+            //   left: 0,
+            //   right: 0,
+            //   child: Container(
+            //     decoration: BoxDecoration(
+            //       gradient: LinearGradient(
+            //         begin: Alignment.bottomCenter,
+            //         end: Alignment.topCenter,
+            //         colors: [
+            //           Colors.black.withOpacity(0.7),
+            //           Colors.transparent,
+            //         ],
+            //       ),
+            //     ),
+            //     padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+            //     child: Text(
+            //       banners[_currentIndex]["title"]!,
+            //       style: TextStyle(
+            //         color: Colors.red.shade200,
+            //         fontSize: 20.0,
+            //         fontWeight: FontWeight.bold,
+            //       ),
+            //       textAlign: TextAlign.center,
+            //     ),
+            //   ),
+            // ),
           ],
         ),
+      ),
+    );
+  }
+
+  Positioned _bannerIndicators() {
+    return Positioned(
+      bottom: 10,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: List.generate(banners.length, (index) {
+          return Container(
+            width: 8.0,
+            height: 8.0,
+            margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 2.0),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: _currentIndex == index ? Colors.red : Colors.grey,
+            ),
+          );
+        }),
+      ),
+    );
+  }
+
+  SizedBox _bannerImages() {
+    return SizedBox(
+      height: 150,
+      child: PageView.builder(
+        controller: _pageController,
+        itemCount: banners.length,
+        onPageChanged: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        itemBuilder: (context, index) {
+          return Image.asset(banners[index]["imageUrl"]!, fit: BoxFit.fill);
+        },
       ),
     );
   }
